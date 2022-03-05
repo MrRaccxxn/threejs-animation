0af38f3689e0fc29056f9ede77e560a910a6285d
byUps <ayrtonskt@gmail.com>
2022-03-05 08:23:12 -0400

    background ready

---
+++ src/components/NeonSign/NeonSign.js
@@ -0,0 +1,14 @@
+export default function NeonSign() {
+  return (
+    <>
+      <div className="scroll text1">
+        <div>
+          A complete new world web to build do you want to be part of it ?
+        </div>
+        <div>
+          A complete new world web to build do you want to be part of it ?
+        </div>
+      </div>
+    </>
+  );
+}
